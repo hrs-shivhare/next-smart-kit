43da367727429c784bbd3b37b5418947cfad1441
Harshvardhan Shivhare <shivhareh@indianoil.in>
2025-05-23 12:03:24 +0530

    feat: update Next.js version, remove unused image, and modify logo link in layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -65,15 +65,15 @@ function ToolbarActionsSearch() {
 function CustomAppTitle() {
   return (
     <div className="flex flex-none items-center gap-4">
-      {/* <Link href="https://rndapps.indianoil.in">
+      <Link href="/">
         <Image
-          src=""
-          alt=""
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
           priority
-          height={}
-          width={}
         />
-      </Link> */}
+      </Link>
 
       <div className="hover:dark:cyan-red-400 whitespace-nowrap bg-gradient-to-b from-cyan-500 to-violet-700 bg-clip-text font-poppins text-3xl font-bold text-transparent hover:from-cyan-600 hover:to-violet-800 dark:from-cyan-200 dark:to-violet-200 dark:hover:from-cyan-200 dark:hover:to-violet-300">
         <Link href="/">Next SMART Kit</Link>
